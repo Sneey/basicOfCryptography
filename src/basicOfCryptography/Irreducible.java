f67449fba218f73f321dafd3f1c74a1d53c088ee
Szczesny <pawel.szczesny@capgemini.com>
2017-01-24 01:31:17 +0100

    added function to generate all possiblePolynomials and reduce number of them for future algorithm

--- src/basicOfCryptography/Irreducible.java
+++ src/basicOfCryptography/Irreducible.java
@@ -1,9 +1,14 @@
 package basicOfCryptography;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
 import utils.IOThings;
 
 public class Irreducible {
-
+	ArrayList<String> result = new ArrayList<String>();
+	
 	public static void main(String[] args) {
 		IOThings io = new IOThings();
 		Irreducible irreducible = new Irreducible();
@@ -21,36 +26,67 @@ public class Irreducible {
 		for(int i=0; i<p;i++){
 			database[i] = Integer.toString(i);
 		}
-	    for(int i=1; i<=m; i++){
-	        String[] result = getAllLists(database, i);
-	        for(int j=0; j<result.length; j++){
-	            System.out.println(result[j]);
-	        }
+	    
+	    String[] resultWithoutReduction = getAllLists(database, m);
+	    for(int j=0; j<resultWithoutReduction.length; j++){	
+	    	System.out.println("b " + resultWithoutReduction[j]);
 	    }
+	    ArrayList<String> resultWithFirstReduction = getAllPolynomialsWithAbsoluteTermAndMDegree(resultWithoutReduction);
+	    for(int j=0; j<resultWithFirstReduction.size(); j++){	
+	    	System.out.println(resultWithFirstReduction.get(j));
+	    }
+	    int last = resultWithFirstReduction.size()-1;
+	    
+	    ArrayList<Integer> prime = findPrimeNumbersUsingSieveOfErasthenes(Integer.parseInt(resultWithFirstReduction.get(last)));
+	    for(int i=0;i<resultWithFirstReduction.size();i++)
+	    	if(prime.contains(Integer.parseInt(resultWithFirstReduction.get(i))))
+	    		result.add(resultWithFirstReduction.get(i));
+	    
+	    System.out.println(result);
 	}
+	
 	public static String[] getAllLists(String[] elements, int lengthOfList)
 	{
-	    //initialize our returned list with the number of elements calculated above
 	    String[] allLists = new String[(int)Math.pow(elements.length, lengthOfList)];
-
-	    //lists of length 1 are just the original elements
+	    
 	    if(lengthOfList == 1) return elements; 
 	    else {
-	        //the recursion--get all lists of length 3, length 2, all the way up to 1
 	        String[] allSublists = getAllLists(elements, lengthOfList - 1);
-
-	        //append the sublists to each element
 	        int arrayIndex = 0;
 
 	        for(int i = 0; i < elements.length; i++){
 	            for(int j = 0; j < allSublists.length; j++){
-	                //add the newly appended combination to the list
 	                allLists[arrayIndex] = elements[i] + allSublists[j];
 	                arrayIndex++;
 	            }
 	        }
 	        return allLists;
 	    }
+	}
+	public ArrayList<String> getAllPolynomialsWithAbsoluteTermAndMDegree(String polynomial[]){
+		ArrayList<String> resultOfReduction = new ArrayList<String>();
+		for(int i=0;i<polynomial.length;i++){
+			if(polynomial[i].charAt(polynomial[i].length()-1)!='0' && polynomial[i].charAt(0)!='0')
+				resultOfReduction.add(polynomial[i]);
+		}
+		return resultOfReduction;
+	}
+	public ArrayList<Integer> findPrimeNumbersUsingSieveOfErasthenes(int maxNumber){
+		ArrayList<Integer> primeNumbers = new ArrayList<Integer>();
+		for(int i=1; i<=maxNumber; i++) 
+			primeNumbers.add(i);
+		int howMany = (int) Math.floor(Math.sqrt(maxNumber));
+		int j;
+		for(int i=2; i<=howMany; i++) {
+			if(primeNumbers.get(i-1)!=0){
+				j = i+i;
+				while(j<=maxNumber) {
+					primeNumbers.set(j-1,0);
+					j += i;
+				}
+			}
+		}
+		return primeNumbers;
 	}
 	public void sayWhichPolynomialIsIrreducible(){
 		
