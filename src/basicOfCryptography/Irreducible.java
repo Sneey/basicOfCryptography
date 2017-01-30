ab8180504a8ddc321637c0bdb16596db0e139d60
Szczesny <pawel.szczesny@capgemini.com>
2017-01-30 08:20:41 +0100

    working algorithm

--- src/basicOfCryptography/Irreducible.java
+++ src/basicOfCryptography/Irreducible.java
@@ -16,7 +16,60 @@ public class Irreducible {
 		int p = io.getNumberFromConsole("p");
 		int m = io.getNumberFromConsole("m");
 		
-		irreducible.generateAllPossiblePolynomials(p, m);
+		//irreducible.generateAllPossiblePolynomials(p, m);
+		System.out.println(irreducible.extendedEuclideanAlgorithm(4864, 3458));
+		irreducible.division("721948327", "84461");
+		
+	}
+	public void division(String x, String y){
+		int n = x.length()-1;
+		int t = y.length()-1;
+		String q = "";
+		
+		for(int j=0;j<(n-t);j++){
+			q+="0";
+		}
+		
+	}
+	
+	public ArrayList<Integer> extendedEuclideanAlgorithm(int a, int b){
+		int d = 0, x = 0, y = 0;
+		int x2 = 1, x1 = 0, y2 = 0, y1 = 1;
+		int q = 0, r = 0;
+		ArrayList<Integer> result = new ArrayList<Integer>();
+		
+		if(b==0) {
+			d = a;
+			x = 1;
+			y = 0;
+			result.add(d);
+			result.add(x);
+			result.add(y);
+			return result;
+		} else {
+			while(b>0){
+				q = (int) a/b;
+				r = a - q*b;
+				x = x2 - q*x1;
+				y = y2 - q*y1;
+				
+				a = b;
+				b = r;
+				x2 = x1;
+				x1 = x;
+				y2 = y1;
+				y1 = y;
+			}
+
+			d = a;
+			x = x2;
+			y = y2;
+			result.add(d);
+			result.add(x);
+			result.add(y);
+			
+			return result;
+		}
 	}
 	
 	public void generateAllPossiblePolynomials(int p, int m){
@@ -27,43 +80,15 @@ public class Irreducible {
 		}
 	    
 	    String[] resultWithoutReduction = getAllLists(database, m);
-	    /*for(int j=0; j<resultWithoutReduction.length; j++){	
+	    for(int j=0; j<resultWithoutReduction.length; j++){	
 	    	System.out.println("b " + resultWithoutReduction[j]);
-	    }*/
+	    }
 	    ArrayList<String> resultWithFirstReduction = getAllPolynomialsWithAbsoluteTermAndMDegree(resultWithoutReduction);
+	    for(int j=0; j<resultWithFirstReduction.size(); j++){	
+	    	System.out.println("b " + resultWithFirstReduction.get(j));
+	    }
 	    
 	    int last = resultWithFirstReduction.size()-1;
-	    
-	    ArrayList<Integer> prime = findPrimeNumbersUsingSieveOfErasthenes(Integer.parseInt(resultWithFirstReduction.get(last)));
-	    for(int i=0;i<resultWithFirstReduction.size();i++)
-	    	if(prime.contains(Integer.parseInt(resultWithFirstReduction.get(i)))){
-	    		result.add(resultWithFirstReduction.get(i));
-	    		resultWithFirstReduction.remove(i);
-	    	}
-	    int counterOfIrreducible = 0;
-	    for(int j=0; j<resultWithFirstReduction.size(); j++){	
-	    	//System.out.println("BASE: " + resultWithFirstReduction.get(j));
-	    	for(int k = 0;k<resultWithoutReduction.length;k++){
-		    	if(Integer.parseInt(resultWithoutReduction[k])<Integer.parseInt(resultWithFirstReduction.get(j))){
-		    		//if(Integer.parseInt(resultWithoutReduction[k])!=0 && Integer.parseInt(resultWithoutReduction[k])!=1){
-		    		if(resultWithoutReduction[k].charAt(0)!='0'){
-		    			if(isDivisible(resultWithFirstReduction.get(j), resultWithoutReduction[k], p))
-		    				continue;
-		    			else{
-		    				counterOfIrreducible++;
-		    				//System.out.println(counterOfIrreducible);
-			    			if(counterOfIrreducible==(resultWithFirstReduction.size()-1)){
-			    				result.add(resultWithFirstReduction.get(j));
-			    				counterOfIrreducible=0;
-			    			}
-		    			}
-		    			//System.out.println("b" + resultWithoutReduction[k]);
-		    		}
-		    			
-		    	}
-	    	}
-	    }
-	    System.out.println("Irreducible: " + result);
 	}
 	
 	public static String[] getAllLists(String[] elements, int lengthOfList)
@@ -109,48 +134,6 @@ public class Irreducible {
 		}
 		return primeNumbers;
 	}
-	public boolean isDivisible(String polynomial1, String polynomial2, int p){
-		CalculationOfBigInteger calcFunc = new CalculationOfBigInteger();
-		//System.out.println("1: " + polynomial1);
-		//System.out.println("2: " + polynomial2);
-		int startingPostion = 0;
-		for(int i=0; i<polynomial2.length();i++){
-			if(Character.getNumericValue(polynomial2.charAt(i))!=0)
-				break;
-			else
-				startingPostion++;
-		}
-		int multiplier=0;
-		//System.out.println("St "+ startingPostion);
-		String afterMultiplication = "";
-		if(Character.getNumericValue(polynomial1.charAt(0))%Character.getNumericValue(polynomial2.charAt(startingPostion))!=0){
-			int inverse = Integer.parseInt(calcFunc.inverseBig(Character.toString(polynomial2.charAt(startingPostion)), Integer.toString(p)));
-			multiplier = (Character.getNumericValue(polynomial1.charAt(0))*inverse)%p;
-		} else {
-			multiplier = Character.getNumericValue(polynomial1.charAt(0))/Character.getNumericValue(polynomial2.charAt(startingPostion));
-		}
-		//System.out.println("Mu "+ startingPostion);
-		for(int i=0;i<polynomial2.length(); i++){
-			afterMultiplication+=(Character.getNumericValue(polynomial2.charAt(i))*multiplier)%p;
-		}
-		//System.out.println("Po 1" + afterMultiplication);
-		
-		String afterSubtraction = "";
-		for(int i=0; i<polynomial2.length();i++){
-			int result = (Character.getNumericValue(polynomial1.charAt(i))-Character.getNumericValue(afterMultiplication.charAt(i)))%p;
-			if(result<0) result+=p;
-			afterSubtraction+=result;
-		}
-		//System.out.println("Po 2" + afterSubtraction);
-		
-		if(Integer.parseInt(afterSubtraction)==0)
-			return true;
-		else
-			if(afterSubtraction.charAt(0)=='0')
-				return false;
-			else
-				isDivisible(afterSubtraction, polynomial2, p);
-		System.out.println("ERROR");
-		return true;
-	}
+	//public boolean isDivisible(String polynomial1, String polynomial2, int p){
+	//}
 }
